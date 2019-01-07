17efc86a6df5d3ae729e2f00111f05edd89bb9c1
Marc Hagen <info@dutchmasterserver.nl>
2019-01-07 18:23:28 +0100

    Removed ;, " > '

--- pds.js
+++ pds.js
@@ -1,41 +1,45 @@
-var tcp = require('../../tcp');
-var instance_skel = require('../../instance_skel');
-var debug;
-var log;
+var tcp = require('../../tcp')
+var instance_skel = require('../../instance_skel')
+var debug
+var log
 
 const PDS_VARIANT_701 = 1
 const PDS_VARIANT_901 = 2
 const PDS_VARIANT_902 = 3
 
 function instance(system, id, config) {
-	var self = this;
+	var self = this
 
-	this.firmwareVersion = "0";
-	this.firmwareVersionIsOver3 = false; // some commands are only working with firmware >= 3
+	this.firmwareVersion = '0'
+	this.firmwareVersionIsOver3 = false // some commands are only working with firmware >= 3
 
 	// super-constructor
-	instance_skel.apply(this, arguments);
+	instance_skel.apply(this, arguments)
 
-	self.actions(); // export actions
+	self.actions() // export actions
 
-	return self;
+	return self
 }
 
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-	debug("updateConfig() destroying and reiniting..");
-	self.destroy();
-	self.init();
-};
+instance.prototype.updateConfig = function (config) {
+	var self = this
+	debug('updateConfig() destroying and reiniting..')
+	self.destroy()
+	self.init()
+}
 
-instance.prototype.init = function() {
-	var self = this;
+instance.prototype.init = function () {
+	var self = this
 
-	debug = self.debug;
-	log = self.log;
+	debug = self.debug
+	log = self.log
 
-	self.states = {};
-	self.init_feedbacks();
+	self.states = {}
+	self.init_feedbacks()
+
+	self.timer = undefined
+	self.init_tcp()
+}
 
 instance.prototype.dataPoller = function () {
 	var self = this
@@ -50,21 +54,21 @@ instance.prototype.dataPoller = function () {
 	)
 }
 
-instance.prototype.init_tcp = function() {
-	var self = this;
-	var receivebuffer = '';
+instance.prototype.init_tcp = function () {
+	var self = this
+	var receivebuffer = ''
 
 	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
+		self.socket.destroy()
+		delete self.socket
 	}
 
 	if (self.config.host) {
-		self.socket = new tcp(self.config.host, 3000);
+		self.socket = new tcp(self.config.host, 3000)
 
 		self.socket.on('status_change', function (status, message) {
-			self.status(status, message);
-		});
+			self.status(status, message)
+		})
 
 		self.socket.on('error', function (err) {
 			debug('Network error', err)
@@ -81,31 +85,32 @@ instance.prototype.init_tcp = function() {
 
 		// separate buffered stream into lines with responses
 		self.socket.on('data', function (chunk) {
-			var i = 0, line = '', offset = 0;
-			receivebuffer += chunk;
+			var i = 0, line = '', offset = 0
+			receivebuffer += chunk
 			while ( (i = receivebuffer.indexOf('\r', offset)) !== -1) {
-				line = receivebuffer.substr(offset, i - offset);
-				offset = i + 1;
-				self.socket.emit('receiveline', line.toString());
+				line = receivebuffer.substr(offset, i - offset)
+				offset = i + 1
+				self.socket.emit('receiveline', line.toString())
 			}
-			receivebuffer = receivebuffer.substr(offset);
-		});
+			receivebuffer = receivebuffer.substr(offset)
+		})
 
 		self.socket.on('receiveline', function (line) {
-			debug("Received line from PDS:", line);
+			debug('Received line from PDS:', line)
 			// check which device and version we have
-			if (line.match(/ShellApp waiting for input/)) self.socket.send(
-				'\r' +
-				'VER -?\r' +
-				'PREVIEW -?\r' +
-				'PROGRAM -?\r' +
-				'LOGOSEL -?\r'
-			)
+			if (line.match(/ShellApp waiting for input/))
+				self.socket.send(
+					'\r' +
+					'VER -?\r' +
+					'PREVIEW -?\r' +
+					'PROGRAM -?\r' +
+					'LOGOSEL -?\r'
+				)
 
 			if (line.match(/VER \d/)) {
-				self.firmwareVersion = line.match(/VER ((?:\d+\.?)+)/)[1];
-				if (parseInt(self.firmwareVersion) >= 3) self.firmwareVersionIsOver3 = true;
-				debug ("version = ", self.firmwareVersion, " is over 3: ", self.firmwareVersionIsOver3);
+				self.firmwareVersion = line.match(/VER ((?:\d+\.?)+)/)[1]
+				if (parseInt(self.firmwareVersion) >= 3) self.firmwareVersionIsOver3 = true
+				debug ('version = ', self.firmwareVersion, ' is over 3: ', self.firmwareVersionIsOver3)
 			}
 
 			if (line.match(/PREVIEW -i\d+/))
@@ -147,32 +152,30 @@ instance.prototype.init_tcp = function() {
 				}
 
 				switch (parseInt(line.match(/-e -(\d+)/)[1])) {
-					case 9999: self.log('error',"Received generic fail error from PDS "+ self.config.label +": "+ line); break;
-					case 9998: self.log('error',"PDS "+ self.config.label +" says: Operation is not applicable in current state: "+ line); break;
-					case 9997: self.log('error',"Received UI related error from PDS "+ self.config.label +", did not get response from device: "+ line); break;
-					case 9996: self.log('error',"Received UI related error from PDS "+ self.config.label +", did not get valid response from device: "+ line); break;
-					case 9995: self.log('error',"PDS "+ self.config.label +" says: Timeout occurred: "+ line); break;
-					case 9994: self.log('error',"PDS "+ self.config.label +" says: Parameter / data out of range: "+ line); break;
-					case 9993: self.log('error',"PDS "+ self.config.label +" says: Searching for data in an index, no matching data: "+ line); break;
-					case 9992: self.log('error',"PDS "+ self.config.label +" says: Checksum didn't match: "+ line); break;
-					case 9991: self.log('error',"PDS "+ self.config.label +" says: Version didn't match: "+ line); break;
-					case 9990: self.log('error',"Received UI related error from PDS "+ self.config.label +", current device interface not supported: "+ line); break;
-					case 9989: self.log('error',"PDS "+ self.config.label +" says: Pointer operation invalid: "+ line); break;
-					case 9988: self.log('error',"PDS "+ self.config.label +" says: Part of command had error: "+ line); break;
-					case 9987: self.log('error',"PDS "+ self.config.label +" says: Buffer overflow: "+ line); break;
-					case 9986: self.log('error',"PDS "+ self.config.label +" says: Initialization is not done (still in progress): "+ line); break;
-					default: self.log('error',"Received unspecified error from PDS "+ self.config.label +": "+ line);
+					case 9999: self.log('error','Received generic fail error from PDS '+ self.config.label +': '+ line) break
+					case 9998: self.log('error','PDS '+ self.config.label +' says: Operation is not applicable in current state: '+ line) break
+					case 9997: self.log('error','Received UI related error from PDS '+ self.config.label +', did not get response from device: '+ line) break
+					case 9996: self.log('error','Received UI related error from PDS '+ self.config.label +', did not get valid response from device: '+ line) break
+					case 9995: self.log('error','PDS '+ self.config.label +' says: Timeout occurred: '+ line) break
+					case 9994: self.log('error','PDS '+ self.config.label +' says: Parameter / data out of range: '+ line) break
+					case 9993: self.log('error','PDS '+ self.config.label +' says: Searching for data in an index, no matching data: '+ line) break
+					case 9992: self.log('error','PDS '+ self.config.label +' says: Checksum didn't match: '+ line) break
+					case 9991: self.log('error','PDS '+ self.config.label +' says: Version didn't match: '+ line) break
+					case 9990: self.log('error','Received UI related error from PDS '+ self.config.label +', current device interface not supported: '+ line) break
+					case 9989: self.log('error','PDS '+ self.config.label +' says: Pointer operation invalid: '+ line) break
+					case 9988: self.log('error','PDS '+ self.config.label +' says: Part of command had error: '+ line) break
+					case 9987: self.log('error','PDS '+ self.config.label +' says: Buffer overflow: '+ line) break
+					case 9986: self.log('error','PDS '+ self.config.label +' says: Initialization is not done (still in progress): '+ line) break
+					default: self.log('error','Received unspecified error from PDS '+ self.config.label +': '+ line)
 				}
 			}
-
-		});
-
+		})
 	}
-};
+}
 
 // Return config fields for web config
 instance.prototype.config_fields = function () {
-	var self = this;
+	var self = this
 
 	return [
 		{
@@ -191,7 +194,7 @@ instance.prototype.config_fields = function () {
 			choices: self.PDS_VARIANT
 		}
 	]
-};
+}
 
 // When module gets deleted
 instance.prototype.destroy = function () {
@@ -203,17 +206,17 @@ instance.prototype.destroy = function () {
 	}
 
 	if (self.socket !== undefined) {
-		self.socket.destroy();
+		self.socket.destroy()
 	}
 
-	self.states = {};
+	self.states = {}
 
-	debug("destroy", self.id);
-};
+	debug('destroy', self.id)
+}
 
-instance.prototype.init_feedbacks = function() {
-	var self = this;
-	var feedbacks = {};
+instance.prototype.init_feedbacks = function () {
+	var self = this
+	var feedbacks = {}
 
 	feedbacks['preview_bg'] = {
 		label: 'Change colors for preview',
@@ -293,35 +296,35 @@ instance.prototype.init_feedbacks = function() {
 		]
 	}
 
-	self.setFeedbackDefinitions(feedbacks);
+	self.setFeedbackDefinitions(feedbacks)
 }
 
-instance.prototype.feedback = function(feedback, bank) {
-	var self = this;
+instance.prototype.feedback = function (feedback, bank) {
+	var self = this
 
 	if (feedback.type === 'program_bg') {
 		if (self.states['program_bg'] === parseInt(feedback.options.input)) {
-			return { color: feedback.options.fg, bgcolor: feedback.options.bg };
+			return { color: feedback.options.fg, bgcolor: feedback.options.bg }
 		}
 	}
 
 	if (feedback.type === 'preview_bg') {
 		if (self.states['preview_bg'] === parseInt(feedback.options.input)) {
-			return { color: feedback.options.fg, bgcolor: feedback.options.bg };
+			return { color: feedback.options.fg, bgcolor: feedback.options.bg }
 		}
 	}
 
 	if (feedback.type === 'logo_bg') {
 		if (self.states['logo_bg'] === parseInt(feedback.options.input)) {
-			return { color: feedback.options.fg, bgcolor: feedback.options.bg };
+			return { color: feedback.options.fg, bgcolor: feedback.options.bg }
 		}
 	}
 
-	return {};
+	return {}
 }
 
-instance.prototype.actions = function(system) {
-	var self = this;
+instance.prototype.actions = function (system) {
+	var self = this
 
 	self.PDS_VARIANT = [
 		{ id: 1, label: 'PDS-701' },
@@ -558,29 +561,29 @@ instance.prototype.actions = function(system) {
 	})
 }
 
-instance.prototype.action = function(action) {
-	var self = this;
+instance.prototype.action = function (action) {
+	var self = this
 
-	var cmd = action.action;
+	var cmd = action.action
 	for (var option in action.options) {
-		if (action.options.hasOwnProperty(option) && action.options[option] !== '') cmd += " -" + option + " " + action.options[option];
+		if (action.options.hasOwnProperty(option) && action.options[option] !== '') cmd += ' -' + option + ' ' + action.options[option]
 	}
-	cmd +="\r";
+	cmd +='\r'
 
 	if (cmd !== undefined) {
 
-		debug('sending tcp',cmd,"to",self.config.host);
+		debug('sending tcp',cmd,'to',self.config.host)
 
 		if (self.socket !== undefined && self.socket.connected) {
-			self.socket.send(cmd);
+			self.socket.send(cmd)
 		} else {
-			debug('Socket not connected :(');
+			debug('Socket not connected :(')
 		}
 
 
 	}
 
-};
+}
 
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+instance_skel.extendedBy(instance)
+exports = module.exports = instance
